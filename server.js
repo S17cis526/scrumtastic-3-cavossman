6a8f9dd0a3838480c3adf430ab45922507a5b93d
Collin Vossman <cvossman@ksu.edu>
2017-03-31 18:08:49 -0500

    Project 2

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('scrumtastic.sqlite3', function(err) {
   if(err) console.error(err);
 });
+var staticFiles = require('./lib/static')
 
 var router = new (require('./lib/route')).Router(db);
+staticFiles.loadDir('./public', router);
+
 
 router.get('/', function(req, res) {
   fs.readFile('public/index.html', function(err, body){
@@ -17,18 +20,20 @@ router.get('/', function(req, res) {
   });
 });
 
-router.get('/app.js', function(req, res) {
-  fs.readFile('public/app.js', function(err, body){
-    res.end(body);
-  });
-});
 
+var project = router.resource('/classes', require('./src/resource/classes.js'))
 
-var project = require('./src/resource/project');
-router.resource('/projects', project);
 
 var migrate = require('./lib/migrate');
 migrate(db, 'migrations', function(err){
+/*
+  fs.readFile('public/Diablo-II-icon.png', function(err, body) {
+    if(err) console.log(err)
+    db.run('INSERT INTO projects (name, description, image) VALUES (?,?,?)', ['diabloII', 'icon', body], function(err){
+      if(err) console.log(err)
+    })
+  })*/
+
 
   var server = new http.Server(function(req, res) {
     router.route(req, res);
@@ -36,6 +41,5 @@ migrate(db, 'migrations', function(err){
   server.listen(PORT, function(){
     console.log("listening on port " + PORT);
   });
-
 
 });
